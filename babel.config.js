670a37ccab192fe8e08a9521355c64108ae4f68a
AHAyayaya <694622038@qq.com>
2021-12-14 17:42:39 +0800

    feat(框架): 集成axios，element-plus

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  // 局部引入element-plus，自动引入css样式
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "element-plus",
+        customStyleName: (name) => {
+          return `element-plus/lib/components/${name}/style/css`;
+        }
+      }
+    ]
+  ],
   presets: ["@vue/cli-plugin-babel/preset"]
 };
